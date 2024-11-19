6f58f823b42bf7992378146ca38bb93255d91b08
64160080 <64160080@go.buu.ac.th>
2024-11-19 06:23:02 +0000

    complete

--- main.php
+++ main.php
@@ -19,6 +19,10 @@ if (!isset($_SESSION['role_id'])) {
 $role_id = $_SESSION['role_id'];
 $user_id = $_SESSION['user_id'];
 
+// โหลดไฟล์ภาษา
+$lang = $_SESSION['lang'] ?? 'en';
+$translations = include("lang/lang_{$lang}.php");
+
 // เลือก Navbar ตามบทบาทของผู้ใช้
 if ($role_id == '1') {
     include 'includes/navbar.php';
@@ -27,7 +31,7 @@ if ($role_id == '1') {
 } elseif ($role_id == '3') {
     include 'includes/maid_navbar.php';
 } else {
-    echo "ไม่สามารถระบุบทบาทของผู้ใช้ได้";
+    echo $translations['error_no_role'];
     exit;
 }
 include 'includes/calendar.php';
@@ -35,40 +39,24 @@ include 'includes/calendar.php';
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h4 mb-0 text-gray-650">Dashboard</h1>
-    </div>
-
     <!-- Content Row -->
     <?php
     date_default_timezone_set('Asia/Bangkok');
-    // การกำหนดค่าในการเชื่อมต่อฐานข้อมูล
-    $servername = "db"; // Use the service name 'db' defined in docker-compose
-    $username = "user"; // User defined in docker-compose
-    $password = "user_password"; // Password defined in docker-compose
+    $servername = "db";
+    $username = "user";
+    $password = "user_password";
     $dbname = "project_maidmanage";
 
-    // การเชื่อมต่อกับ MySQL
     $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // ตรวจสอบการเชื่อมต่อ
     if ($conn->connect_error) {
-        die("การเชื่อมต่อล้มเหลว: " . $conn->connect_error);
+        die($translations['db_connection_failed'] . ": " . $conn->connect_error);
     }
 
-    // คำนวณวันจันทร์ของสัปดาห์ปัจจุบัน
     $today = date('Y-m-d');
     $monday = date('Y-m-d', strtotime('monday this week', strtotime($today)));
-
-    // คำนวณวันอาทิตย์ของสัปดาห์ถัดไป
     $nextSunday = date('Y-m-d', strtotime('sunday next week', strtotime($monday)));
-
-    // เงื่อนไขการกรองข้อมูลตามบทบาท
     $user_condition = $role_id == '3' ? "AND user_id = '$user_id'" : "";
 
-    // นับจำนวนห้องที่เริ่มต้นในช่วง 7 วันจากวันจันทร์ถึงวันอาทิตย์
     $sql_weekly_room_count = "SELECT COUNT(task_id) AS room_count 
                               FROM task 
                               WHERE start_date >= '$monday' AND start_date <= '$nextSunday' 
@@ -87,24 +75,22 @@ include 'includes/calendar.php';
     // นับจำนวนห้องที่ทำความสะอาดแล้วในช่วง 7 วันจากวันจันทร์ถึงวันอาทิตย์
     $sql_cleaned_room_count = "SELECT COUNT(task_id) AS cleaned_rooms 
                                FROM task 
-                           WHERE (status_id = 1 OR status_id IS NULL)  -- 1 คือ 'Ready' หรือ NULL
-                           AND (toilet_status_id = 1 OR toilet_status_id IS NULL)  -- 1 คือ 'Ready' หรือ NULL
+                               WHERE (status_id = 1 OR status_id IS NULL)  
+                               AND (toilet_status_id = 1 OR toilet_status_id IS NULL)
                                AND start_date >= '$monday' AND start_date <= '$nextSunday' 
                                $user_condition";
     $result_cleaned_room_count = $conn->query($sql_cleaned_room_count);
     $cleaned_room_count = $result_cleaned_room_count->fetch_assoc()['cleaned_rooms'];
 
-    // นับจำนวนห้องที่เสร็จสมบูรณ์ในช่วง 7 วันจากวันจันทร์ถึงวันอาทิตย์
     $sql_complete_room_count = "SELECT COUNT(task_id) AS complete_rooms 
                                 FROM task 
-                           WHERE (status_id = 1 OR status_id IS NULL)  -- 1 คือ 'Ready' หรือ NULL
-                           AND (toilet_status_id = 1 OR toilet_status_id IS NULL)  -- 1 คือ 'Ready' หรือ NULL
+                                WHERE (status_id = 1 OR status_id IS NULL)  
+                                AND (toilet_status_id = 1 OR toilet_status_id IS NULL)
                                 AND start_date >= '$monday' AND start_date <= '$nextSunday' 
                                 $user_condition";
     $result_complete_room_count = $conn->query($sql_complete_room_count);
     $complete_room_count = $result_complete_room_count->fetch_assoc()['complete_rooms'];
 
-    // หาหมายเลขชั้นที่มีจำนวนห้องมากที่สุด 3 อันดับแรกในช่วง 7 วันจากวันจันทร์ถึงวันอาทิตย์
     $sql_top_floor_count = "SELECT floor_id, COUNT(*) AS floor_count
                             FROM task
                             WHERE start_date >= '$monday' AND start_date <= '$nextSunday'
@@ -120,7 +106,6 @@ include 'includes/calendar.php';
             $floor_ids[] = $row['floor_id'];
         }
     }
-
     ?>
 
     <div class="row">
@@ -132,12 +117,11 @@ include 'includes/calendar.php';
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"
                                 style="font-family: Prompt; font-size: 14.5px;">
-                                จำนวนห้องที่ใช้สัปดาห์นี้
+                                <?php echo $translations['rooms_used_this_week']; ?>
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <?php
-                                // แสดงจำนวนห้องที่ใช้สัปดาห์นี้
-                                echo '<span style="font-family: Prompt;">' . ($weekly_room_count ? $weekly_room_count : '0') . ' ห้อง</span>';
+                                echo '<span style="font-family: Prompt;">' . ($weekly_room_count ? $weekly_room_count : '0') . ' ' . $translations['rooms'] . '</span>';
                                 ?>
                             </div>
                         </div>
@@ -157,12 +141,11 @@ include 'includes/calendar.php';
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1"
                                 style="font-family: Prompt; font-size: 14.5px;">
-                                ทำความสะอาดแล้ว
+                                <?php echo $translations['cleaned_rooms']; ?>
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <?php
-                                // แสดงจำนวนห้องที่ทำความสะอาดแล้ว
-                                echo '<span style="font-family: Prompt;">' . ($cleaned_room_count ? $cleaned_room_count : '0') . ' ห้อง</span>';
+                                echo '<span style="font-family: Prompt;">' . ($cleaned_room_count ? $cleaned_room_count : '0') . ' ' . $translations['rooms'] . '</span>';
                                 ?>
                             </div>
                         </div>
@@ -182,13 +165,12 @@ include 'includes/calendar.php';
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1"
                                 style="font-family: Prompt; font-size: 14.5px;">
-                                คิดเป็น
+                                <?php echo $translations['completion_percentage']; ?>
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
                                     <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
                                         <?php
-                                        // เริ่มต้นค่าเปอร์เซ็นต์เป็น 0.0
                                         $percentage = 0.0;
 
                                         // ตรวจสอบว่าผลลัพธ์ของการคำนวณมีข้อมูลหรือไม่
@@ -198,8 +180,6 @@ include 'includes/calendar.php';
                                             // คำนวณเปอร์เซ็นต์
                                             $percentage = ($complete_rooms / $total_room_count) * 100;
                                         }
-
-                                        // รูปแบบเปอร์เซ็นต์
                                         $formatted_percentage = number_format($percentage, 2);
                                         ?>
                                         <span style="font-family: Prompt;"><?php echo $formatted_percentage; ?> %</span>
@@ -231,11 +211,10 @@ include 'includes/calendar.php';
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"
                                 style="font-family: Prompt; font-size: 14.5px;">
-                                ชั้นที่ต้องดูแลเป็นพิเศษ
+                                <?php echo $translations['special_care_floors']; ?>
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <?php
-                                // แสดงหมายเลขชั้นที่ต้องดูแลเป็นพิเศษ
                                 echo implode(', ', $floor_ids);
                                 ?>
                             </div>
@@ -254,11 +233,9 @@ include 'includes/calendar.php';
         <!-- Calendar -->
         <div class="col-xl-8 col-lg-7">
             <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Calendar</h6>
+                    <h6 class="m-0 font-weight-bold text-primary"><?php echo $translations['calendar']; ?></h6>
                 </div>
-                <!-- Card Body -->
                 <div class="card-body">
                     <div id='calendar'></div>
                 </div>
@@ -268,11 +245,9 @@ include 'includes/calendar.php';
         <!-- Today Notify -->
         <div class="col-xl-4 col-lg-5">
             <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Today Notify</h6>
+                    <h6 class="m-0 font-weight-bold text-primary"><?php echo $translations['today_notify']; ?></h6>
                 </div>
-                <!-- Card Body -->
                 <div class="card-body">
                     <div class="accordion" id="taskAccordion">
                         <?php
@@ -329,8 +304,6 @@ include 'includes/calendar.php';
 
                         // รันคำสั่ง SQL
                         $result = $conn->query($sql);
-
-                        // ตรวจสอบว่ามีข้อมูลหรือไม่
                         if ($result->num_rows > 0) {
                             while ($row = $result->fetch_assoc()) {
                                 echo '<div class="card">';
@@ -411,7 +384,7 @@ include 'includes/calendar.php';
                                 echo '</div>';
                             }
                         } else {
-                            echo "No tasks found for today.";
+                            echo $translations['no_tasks_today'];
                         }
                         ?>
                     </div>
